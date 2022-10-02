496d2640311f4cb77bf1ced9e5b701a4d5bb732a
Userwcl1 <111431961+Userwcl1@users.noreply.github.com>
2022-10-02 13:49:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
-#city = os.environ['CITY'] #敏敏
+city = os.environ['CITY'] #敏敏
 cityw = os.environ['CITYW'] #我
 birthday = os.environ['BIRTHDAY']
 
@@ -16,17 +16,17 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_w=os.environ["USER_W"] #我
-#user_id = os.environ["USER_ID"] #敏敏
+user_id = os.environ["USER_ID"] #敏敏
 template_id = os.environ["TEMPLATE_ID"]
 
 #敏敏的天气
-'''
+
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
-'''
+
 
 #我的天气
 
@@ -52,6 +52,12 @@ def get_words():
     return get_words()
   return words.json()['data']['text']
 
+def get_wordsw():
+  wordsw = requests.get("https://api.shadiao.pro/chp")
+  if wordsw.status_code != 200:
+    return get_wordsw()
+  return wordsw.json()['data']['text']
+
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
@@ -59,13 +65,13 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-#wea, temperature = get_weather()
+wea, temperature = get_weather()
 weaw,temperaturew = get_weatherw()
-#data = {"city":{"value":city},"weather":{"value":wea},"temperature":{"value":temperature},
-   #     "love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}} #敏敏
-dataw = {"cityw":{"value":cityw},"weatherw":{"value":weaw},"temperaturew":{"value":temperaturew},"words":{"value":get_words(), "color":get_random_color()}} #我
-#res = wm.send_template(user_id, template_id, data) #敏敏
+data = {"city":{"value":city},"weather":{"value":wea},"temperature":{"value":temperature},
+      "love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}} #敏敏
+dataw = {"cityw":{"value":cityw},"weatherw":{"value":weaw},"temperaturew":{"value":temperaturew},"wordsw":{"value":get_wordsw(), "color":get_random_color()}} #我
+res = wm.send_template(user_id, template_id, data) #敏敏
 resw = wm.send_template(user_w, template_id, dataw) #我
-#print(res)
+print(res)
 print(resw)
 
